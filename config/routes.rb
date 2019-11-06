fb3ab995d5d2580a7f030b3a04063683e41d10bb
MamiKosaka <m.kosaka.0228@gmail.com>
2019-11-06 22:11:32 +0900

    edit_incre_search

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root "groups#index"
-  resources :users, only: [:edit, :update]
+  # root "users#index"
+  resources :users, only: [:edit, :update, :index]
   resources :groups, only: [:new, :create, :update, :edit] do
     resources :messages, only: [:index, :create]
   end
